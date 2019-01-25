2d0c4f13e981e7a75ab4a736dd7307ed41350b8f
Tian du Toit <tian@hmsnz.com>
2019-01-25 16:56:21 +1300

    additional data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -36,7 +36,26 @@ export class Homepage extends Component {
         { id: 112, width: 40, length: 40, coords: [540, 0], isContainer: true },
         { id: 113, width: 40, length: 40, coords: [580, 0], isContainer: true },
         { id: 114, width: 40, length: 40, coords: [620, 0], isContainer: true },
-        { id: 115, width: 40, length: 40, coords: [660, 0], isContainer: true }
+        { id: 115, width: 40, length: 40, coords: [660, 0], isContainer: true },
+        { id: 116, width: 40, length: 40, coords: [700, 0], isContainer: true },
+        { id: 117, width: 40, length: 40, coords: [740, 0], isContainer: true },
+        { id: 118, width: 40, length: 40, coords: [780, 0], isContainer: true },
+        { id: 119, width: 20, length: 40, coords: [820, 0], isContainer: true },
+        { id: 120, width: 20, length: 40, coords: [840, 0], isContainer: true },
+        { id: 121, width: 20, length: 40, coords: [860, 0], isContainer: true },
+        { id: 122, width: 20, length: 40, coords: [880, 0], isContainer: true },
+        { id: 123, width: 20, length: 40, coords: [900, 0], isContainer: true },
+        { id: 124, width: 20, length: 40, coords: [920, 0], isContainer: true },
+        { id: 125, width: 20, length: 40, coords: [940, 0], isContainer: true },
+        { id: 126, width: 20, length: 40, coords: [960, 0], isContainer: true },
+        { id: 127, width: 20, length: 40, coords: [980, 0], isContainer: true },
+        {
+          id: 128,
+          width: 140,
+          length: 160,
+          coords: [1000, 0],
+          isContainer: true
+        }
       ]
     }
     this.handleClick = this.handleClick.bind(this)
